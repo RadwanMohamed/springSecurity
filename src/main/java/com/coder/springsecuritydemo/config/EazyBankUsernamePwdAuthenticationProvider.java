6c3a4eca6a6ff98330461135c2ca17423187314d
Radwan <radwan.mohamed@jobzella.com>
2022-09-07 13:16:36 +0200

    5 - Implementing and Customizing the Authentication Provider inside our application

---
+++ src/main/java/com/coder/springsecuritydemo/config/EazyBankUsernamePwdAuthenticationProvider.java
@@ -0,0 +1,47 @@
+package com.coder.springsecuritydemo.config;
+
+
+import com.coder.springsecuritydemo.model.CustomerEntity;
+import com.coder.springsecuritydemo.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class EazyBankUsernamePwdAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String usrname = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        CustomerEntity customer = customerRepository.findByEmail(usrname);
+        if (customer != null){
+            System.out.println("ffff");
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            authorities.add(new SimpleGrantedAuthority(customer.getRole()));
+            return  new UsernamePasswordAuthenticationToken(usrname,password,authorities);
+        }else {
+            throw new BadCredentialsException("invalid");
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
